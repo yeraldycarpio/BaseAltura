6dbfefbca6a5a02d0ed9e630680972f0559c3aa8
MINEDUCYT <MINEDUCYT@DESKTOP-OAO2BUA>
2023-07-03 21:30:36 -0600

    Agregar archivos de proyecto.

---
+++ BaseAltura/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BaseAltura;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ BaseAltura/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace BaseAltura;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void BtnCalcular_Clicked(object sender, EventArgs e)
+	{
+		if  (!string.IsNullOrEmpty(Base.Text) || (!String.IsNullOrEmpty(Altura.Text)))
+		{
+			double xBase, xAltura, xResultado;
+
+			xBase = Convert.ToDouble(Base.Text);
+			xAltura = Convert.ToDouble(Altura.Text);
+
+			xResultado = xBase * xAltura;
+
+			calcular.Text=Convert.ToString(xResultado);
+		}
+		else
+		{
+			DisplayAlert("Eror", "Ingrese la informacion correcta", "OK");
+		}
+	}
+}
+
